9a71314ddd04e58f2476e90ca98fae88747cd880
rice <minhrice.dev@gmail.com>
2024-08-21 09:25:12 +0700

    Counter Problem

---
+++ Counter/index.js
@@ -0,0 +1,10 @@
+let count = 0;
+const $countEl = document.querySelector('#count');
+
+document
+    .querySelector('button')
+    .addEventListener('click', () => {
+        // Fix the bug in the next line.
+        count = count - 1;
+        $countEl.textContent = count;
+    });
