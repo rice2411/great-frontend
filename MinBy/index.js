f5bd0e6610e830fc061e4309dbb2a8071be25a8f
rice <minhrice.dev@gmail.com>
2024-08-21 09:37:14 +0700

    Min By Problem

---
+++ MinBy/index.js
@@ -0,0 +1,15 @@
+function minBy(array, iteratee) {
+    let result, computed;
+
+    // Iterate through array to find the minimum `result`.
+    for (const value of array) {
+        const current = iteratee(value);
+        // Check whether `computed` is assigned any value yet then compare with `current`, else assign an initial value to `computed` where `current` is not `null`.
+        if (current != null && (computed === undefined || current < computed)) {
+            computed = current; // Store the calculated value of the current `result`.
+            result = value;
+        }
+    }
+
+    return result;
+}
