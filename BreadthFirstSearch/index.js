33632f4cbcb9be01bd067746c754ebf0e486b3e7
PhucLH <longdh@twinger.vn>
2024-08-22 09:19:39 +0700

    Breadth-first Search Problem

---
+++ BreadthFirstSearch/index.js
@@ -0,0 +1,99 @@
+/**
+ * @param {Record<string, Array<string>>} graph The adjacency list representing the graph.
+ * @param {string} source The source node to start traversal from. Has to be a valid node if graph is non-empty.
+ * @return {Array<string>} A BFS-traversed order of nodes.
+ */
+export default function breadthFirstSearch(graph, source) {
+    // If there are no nodes in the graph, just return an empty array
+    if (Object.keys(graph).length === 0) {
+        return [];
+    }
+
+    // Create a queue to store the nodes to be visited.
+    // Add the root node since we're doing a level-order BFS.
+    const queue = new Queue();
+    queue.enqueue(source);
+
+    // Initialize a set that tracks visited nodes.
+    const visited = new Set();
+
+    // While there are nodes to visit.
+    while (!queue.isEmpty()) {
+        // Dequeue the node at the front of the queue.
+        const node = queue.dequeue();
+
+        // Mark the node as visited.
+        visited.add(node);
+
+        // Enqueue the neighbors of the current node.
+        graph[node].forEach((neighbor) => {
+            // Skip nodes that have already been visited.
+            if (visited.has(neighbor)) {
+                return;
+            }
+
+            queue.enqueue(neighbor);
+        });
+    }
+
+    // The visited nodes is the traversal order.
+    return Array.from(visited);
+}
+
+/*  Auxiliary classes */
+
+/**
+ * A Queue class with O(1) enqueue and dequeue is provided for you.
+ * You can use it directly should you wish to.
+ *
+ * Example usage:
+ * const q = new Queue();
+ * q.enqueue('a');
+ * q.enqueue('b');
+ * q.dequeue(); //'a'
+ * q.isEmpty(); // False
+ */
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    enqueue(item) {
+        const newNode = new Node(item);
+        if (this.isEmpty()) {
+            this.head = newNode;
+        } else if (this.tail) {
+            this.tail.next = newNode;
+        }
+        this.tail = newNode;
+        this.length++;
+    }
+
+    dequeue() {
+        if (this.isEmpty() || !this.head) {
+            return null;
+        } else {
+            const removedNode = this.head;
+            this.head = this.head.next;
+            removedNode.next = null;
+            this.length--;
+            if (this.isEmpty()) {
+                this.tail = null;
+            }
+            return removedNode.value;
+        }
+    }
+}
